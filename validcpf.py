6f4e377db345d64c9abde3e8966910bce88e43cb
lucas-tadeu-dev <lucastmb2016@gmail.com>
2022-06-25 12:06:46 -0300

    commit val cpf

---
+++ validcpf.py
@@ -0,0 +1,37 @@
+# Loop infinito
+while True:
+    # cpf = '16899535009'
+    cpf = input('Digite um CPF: ')
+    novo_cpf = cpf[:-2]                 # Elimina os dois últimos digitos do CPF
+    reverso = 10                        # Contador reverso
+    total = 0
+
+    # Loop do CPF
+    for index in range(19):
+        if index > 8:                   # Primeiro índice vai de 0 a 9,
+            index -= 9                  # São os 9 primeiros digitos
+
+        total += int(novo_cpf[index]) * reverso  # Valor total da multiplicação
+
+        reverso -= 1                    # Decrementa o contador reverso
+        if reverso < 2:
+            reverso = 11
+            d = 11 - (total % 11)
+
+            if d > 9:                   # Se o digito for > que 9 o valor é 0
+                d = 0
+            total = 0                   # Zera o total
+            novo_cpf += str(d)          # Concatena o digito gerado no novo cpf
+
+    # Tirei as sequencias, porque descobri que elas dão erro na conta 
+    sequencia = novo_cpf == str(novo_cpf[0]) * len(cpf)
+
+    
+    if cpf == novo_cpf and not sequencia:
+        print('Válido')
+    else:
+        print('Inválido')
+
+
+
+  
